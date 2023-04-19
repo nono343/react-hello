8650546fa3c53e8c2674a57f18e993dacf5b0338
Antonio Gamero <125345806+nono343@users.noreply.github.com>
2023-04-19 19:06:32 +0000

    semaforo 1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [colorBtnRed, setColorBtnRed] = useState("btn-danger");
+  const [colorBtnOrange, setColorBtnOrange] = useState("btn-warning");
+  const [colorBtnGreen, setColorBtnGreen] = useState("btn-success");
+  const [activeBtn, setActiveBtn] = useState("");
+
+  function changeColor(btnName) {
+    if (activeBtn !== btnName) {
+      setActiveBtn(btnName);
+      setColorBtnRed("btn-danger");
+      setColorBtnOrange("btn-warning");
+      setColorBtnGreen("btn-success");
+
+      if (btnName === "red") {
+        setColorBtnRed("btn-red");
+      } else if (btnName === "orange") {
+        setColorBtnOrange("btn-orange");
+      } else if (btnName === "green") {
+        setColorBtnGreen("btn-green");
+      }
+    }
+  }
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "200px", width: "100px", backgroundColor: "black" }}>
+        <div id="redLight" className={"btn " + colorBtnRed} onClick={() => changeColor("red")}></div>
+        <div id="orangeLight" className={"btn " + colorBtnOrange} onClick={() => changeColor("orange")}></div>
+        <div id="greenLight" className={"btn " + colorBtnGreen} onClick={() => changeColor("green")}></div>
+      </div>
+  );
 };
 
 export default Home;
